572949e5b191f1270a83c977b116d2e2dbb8bc71
dcalvo4-dev <56971161+dcalvo4-dev@users.noreply.github.com>
2021-08-24 21:57:18 -0700

    Switch

---
+++ js/first-focus.js
@@ -0,0 +1,4 @@
+/* Focus on the first link after content is loaded */
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementsByTagName('a')[0].focus();
+})
---
+++ js/theme-switch.js
@@ -0,0 +1,57 @@
+/*
+    Functions to initialize theme. For first time visitors, the theme will be set to light.
+    Once the user updates the theme, their choice will be stored into local storage.
+*/
+document.addEventListener('DOMContentLoaded', () => {
+    initialize_theme_switch();
+})
+
+/*
+    Initialize event listener for theme switch.
+    Loads the appropriate theme based on localStorage.
+    Default: Light Setting
+*/
+function initialize_theme_switch () {
+    let sw = document.getElementById('theme-switch');
+    let light = 'light';
+    let dark = 'dark';
+
+    /* 
+        Initiate theme based on localStorage.
+        First time visitors will be in light mode.
+    */
+    if (get_local_style() === 'null') {
+        set_local_style(light);
+        update_theme(light);
+    }
+    else {
+        set_local_style(dark);
+        update_theme(dark);
+    }
+
+    /* Cause switch to alternate between light and dark */
+    sw.addEventListener('click', () => {
+        if (get_local_style() === light) {
+            set_local_style(dark);
+            update_theme(dark);
+        }
+        else {
+            set_local_style(light);
+            update_theme(light);
+        }
+    })
+}
+
+function get_local_style() {
+    return localStorage.getItem('style');
+}
+
+function set_local_style(style) {
+    localStorage.setItem('style', style);
+}
+
+/* Update current page's theme based on the theme set in localtorage */
+function update_theme(theme) {
+    let this_theme = document.getElementById('theme-switcher-id');
+    this_theme.href = `../css/themes/${theme}.css`;
+}
