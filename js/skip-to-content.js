15c8a885612bca5926befe1c305177c934612103
Daniel <dandcalvo@gmail.com>
2022-03-02 15:18:18 -0800

    Update theme switch

--- js/skip-to-content.js
+++
@@ -1,4 +0,0 @@
-/* Focus on the first link after content is loaded */
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementsByTagName('a')[0].focus();
-})
--- js/theme-switch.js
+++ js/theme-switch.js
@@ -1,58 +1,34 @@
-/*
-    Functions to initialize theme. For first time visitors, the theme will be set to light.
-    Once the user updates the theme, their choice will be stored into local storage.
-*/
-document.addEventListener('DOMContentLoaded', () => {
-    initialize_theme_switch();
-})
+let body = document.querySelector('body');
+let sw = document.getElementById('theme-switch');
+let style = localStorage.getItem('style');
 
-/*
-    Initialize event listener for theme switch.
-    Loads the appropriate theme based on localStorage.
-    Default: Light Setting
+function enableDarkMode() {
+    localStorage.setItem('style', 'dark');
+    body.classList.add('dark-theme');
+}
+
+function disableDarkMode() {
+    localStorage.setItem('style', '');
+    body.classList.remove('dark-theme');
+}
+
+/* 
+    Initiate theme based on localStorage.
+    First time visitors will be in light mode.
 */
-function initialize_theme_switch () {
-    let light = 'light';
-    let dark = 'dark';
-    
-    /* 
-        Initiate theme based on localStorage.
-        First time visitors will be in light mode.
-    */
-    if (get_local_style() === 'null' || get_local_style() === light) {
-        set_local_style(light);
-        update_theme(light);
+if (style === 'dark') {
+    enableDarkMode();
+}
+
+/* Cause switch to alternate between light and dark */
+sw.addEventListener('click', () => {
+    console.log('hello');
+    style = localStorage.getItem('style');
+
+    if (style === '') {
+        enableDarkMode();
     }
     else {
-        set_local_style(dark);
-        update_theme(dark);
+        disableDarkMode();
     }
-
-    let sw = document.getElementById('theme-switch');
-
-    /* Cause switch to alternate between light and dark */
-    sw.addEventListener('click', () => {
-        if (get_local_style() === light) {
-            set_local_style(dark);
-            update_theme(dark);
-        }
-        else {
-            set_local_style(light);
-            update_theme(light);
-        }
-    })
-}
-
-function get_local_style() {
-    return localStorage.getItem('style');
-}
-
-function set_local_style(style) {
-    localStorage.setItem('style', style);
-}
-
-/* Update current page's theme based on the theme set in localtorage */
-function update_theme(theme) {
-    let this_theme = document.getElementById('theme-switcher-id');
-    this_theme.href = `../css/themes/${theme}.css`;
-}
+});
