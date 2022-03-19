5e265f223e189365d388f346ee8e8b0545087f3f
Daniel <dcalvo@leomail.tamuc.edu>
2022-03-19 13:31:05 -0700

    Implement mobile nav bar

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,10 +1,13 @@
 let nav_button = document.getElementById('nav-toggle');
+let mobile_nav_bar = document.querySelector('.mobile-nav-bar');
 
 nav_button.addEventListener('click', () => {
     if (nav_button.classList.contains('active')) {
         nav_button.classList.remove('active');
+        mobile_nav_bar.classList.remove('active');
     }
     else {
         nav_button.classList.add('active');
+        mobile_nav_bar.classList.add('active');
     }
 });
