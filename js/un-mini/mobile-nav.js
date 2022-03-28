1d6933846426f61e4fb4c4d94d6ea2eb08fcf2ec
Daniel <dandcalvo@gmail.com>
2022-03-28 13:20:50 -0700

    Simplified mobile nav JS

--- js/un-mini/mobile-nav.js
+++ js/un-mini/mobile-nav.js
@@ -2,12 +2,6 @@ let nav_button = document.getElementById('nav-toggle');
 let mobile_nav_bar = document.querySelector('.mobile-nav-bar');
 
 nav_button.addEventListener('click', () => {
-    if (nav_button.classList.contains('active')) {
-        nav_button.classList.remove('active');
-        mobile_nav_bar.classList.remove('active');
-    }
-    else {
-        nav_button.classList.add('active');
-        mobile_nav_bar.classList.add('active');
-    }
+    nav_button.classList.toggle('active');
+    mobile_nav_bar.classList.toggle('active');
 });
