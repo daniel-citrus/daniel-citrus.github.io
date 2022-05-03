410db06c4ae6a95803f4c84aa9f1929bf3add943
Daniel <dandcalvo@gmail.com>
2022-05-03 16:44:33 -0700

    favicon

--- js/mobile-nav.js
+++
@@ -1,7 +0,0 @@
-let nav_button = document.getElementById('nav-toggle');
-let mobile_nav_bar = document.querySelector('.mobile-nav-bar');
-
-nav_button.addEventListener('click', () => {
-    nav_button.classList.toggle('active');
-    mobile_nav_bar.classList.toggle('active');
-});
